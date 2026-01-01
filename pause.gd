30701a31d8245a81f9de7746bfdda7ea3e7d37d2
Ks-Marbs <nykolask11@gmail.com>
2026-01-01 16:29:21 -0300

    phone testing further

--- pause.gd
+++ pause.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if $lef.button_pressed:
+	if button_pressed:
 		Input.action_press("esc")
 	else:
 		Input.action_release("esc")
