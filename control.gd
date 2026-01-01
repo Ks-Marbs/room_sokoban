6058353ca2f230c1c2a2e79462bc91385d9f4899
Ks-Marbs <nykolask11@gmail.com>
2026-01-01 17:20:57 -0300

    please say the controller works

--- control.gd
+++ control.gd
@@ -3,6 +3,10 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	if DisplayServer.is_touchscreen_available():
+		visible = true
+	else:
+		visible = false
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- pause.gd
+++ pause.gd
@@ -3,13 +3,15 @@ extends TextureButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	if DisplayServer.is_touchscreen_available():
+		scale = Vector2.ONE
+	else:
+		scale = Vector2.ONE * 0.75
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if button_pressed:
-		Input.action_press("esc")
-	else:
-		Input.action_release("esc")
+	if self.button_pressed:
+		Global.toggle = true
 	pass
